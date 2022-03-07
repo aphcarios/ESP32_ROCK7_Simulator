821ba2f2e4c23ff95481f9e1dae8c62925c09258
Basel Korj <baselanwarkorj@gmail.com>
2022-03-07 14:23:52 +0400

    bug fixes

--- src/main.cpp
+++ src/main.cpp
@@ -13,12 +13,12 @@ PubSubClient mqttClient(client);
 
 void initWiFi();
 void initMQTT(String token);
-void processGateway(String message);
+void processGateway(char message[]);
 void replyToGateway(void);
 void postData(char data[]);
 
 bool binarySession = false;
-char data[5] = "";
+char data[4];
 int dataLength = 0;
 
 String api = "http://159.223.77.171:8080/api/whiskers/Device/test/6e35c4d4-9e1e-4e84-bcae-fe762cc1e0c3";
@@ -74,7 +74,7 @@ void loop()
       message[i] = temp;
     }
   }
-  
+
   processGateway(message);
 }
 
@@ -93,10 +93,11 @@ void initWiFi()
   Serial.println(WiFi.localIP());
 }
 
-void processGateway(String message)
+void processGateway(char msg[])
 {
   if (!binarySession)
   {
+    String message = msg;
     if (message == "AT" || message == "AT&K0")
     {
       Serial.println("Received: " + message);
@@ -132,54 +133,62 @@ void processGateway(String message)
   else
   {
     binarySession = false;
-    // data = message.substring(0, dataLength);
-    message.substring(0, dataLength).toCharArray(data, dataLength);
+    for (int i = 0; i < dataLength; i++)
+    {
+      data[i] = msg[i];
+    }
     Serial2.print("\r\n0\r\n");
     Serial2.print("\r\nOK\r\n");
     Serial.print("Data: ");
-    Serial.println(data);
     Serial.println("Reply: OK\n");
   }
 }
 
-void callback(char* topic, byte* message, unsigned int length) {
-    Serial.print("Message arrived on topic: ");
-    Serial.print(topic);
-    Serial.print(". Message: ");
-    String messageTemp;
-    
-    for (int i = 0; i < length; i++) {
-        Serial.print((char)message[i]);
-        messageTemp += (char)message[i];
-    }
-    Serial.println(messageTemp);
+void callback(char *topic, byte *message, unsigned int length)
+{
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
 
-    if(String(topic) == "tracking/actual_position"){
-        
-    }
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println(messageTemp);
+
+  if (String(topic) == "tracking/actual_position")
+  {
+  }
 }
 
-void initMQTT(String token){
+void initMQTT(String token)
+{
   // MQTT Broker
   const char *mqttBroker = "134.122.92.235";
   // const char *mqttBroker = "whiskershub.alpha.aphcarios.com";
   const char *mqttClientId = "esp32Sim";
   const int mqttPort = 1883;
-  const char *user = "BrNjFhYr9g7kbTrw43g7"; 
-  const char *password = ""; 
+  const char *user = "BrNjFhYr9g7kbTrw43g7";
+  const char *password = "";
 
   mqttClient.setServer(mqttBroker, mqttPort);
   mqttClient.setCallback(callback);
 
-  while (!mqttClient.connected()) {
+  while (!mqttClient.connected())
+  {
     Serial.printf("The client %s connects to the public mqtt broker\n", mqttClientId);
-    if (mqttClient.connect(mqttClientId, token.c_str(), password)) {
-        Serial.println("Public emqx mqtt broker connected");
-        mqttClient.subscribe("tracking/actual_position");
-    } else {
-        Serial.print("failed with state ");
-        Serial.print(mqttClient.state());
-        delay(2000);
+    if (mqttClient.connect(mqttClientId, token.c_str(), password))
+    {
+      Serial.println("Public emqx mqtt broker connected");
+      mqttClient.subscribe("tracking/actual_position");
+    }
+    else
+    {
+      Serial.print("failed with state ");
+      Serial.print(mqttClient.state());
+      delay(2000);
     }
   }
 }
@@ -187,14 +196,24 @@ void postData(char data[])
 {
   String device = "";
   String accessToken = "";
-  char sender = data[0] - '0';
-  Serial.println("Sender " + String(sender));
+  int sender = data[0];
+  int trigger = data[1];
+  int temperature = data[2];
+  int battery = data[3];
 
-  if(sender == 0){
+  Serial.println(sender);
+  Serial.println(trigger);
+
+  Serial.println(temperature);
+  Serial.println(battery);
+
+  if (sender == 0)
+  {
     device = "S0006";
     accessToken = "scscscdwrrerer";
   }
-  else if (sender == 1){
+  else if (sender == 1)
+  {
     device = "PV0010";
     accessToken = "BrNjFhYr9g7kbTrw43g7";
   }
@@ -205,17 +224,20 @@ void postData(char data[])
   Serial.println("Device " + device);
   Serial.println("Token " + accessToken);
 
-  bool trigger;
-
-  if(data[1] - '0' == 0) trigger = false;
-  else if(data[1] - '0' == 1) trigger = true;
+  // if (data[1] - '0' == 0)
+  //   trigger = false;
+  // else if (data[1] - '0' == 1)
+  //   trigger = true;
 
   String deviceJson = "{\"device\":" + device + "}";
-  String telemetryJson =  "{\"trigger\":" + String(trigger) + 
-                          ",\"temperature\":" + String((int)data[2]) +
-                          ",\"battery_level\":" + String((int)data[3]) + 
-                          "}";
-  
+  String telemetryJson = "{\"trigger\":" + String(trigger) +
+                         ",\"temperature\":" + String(temperature) +
+                         ",\"battery_level\":" + String(battery) +
+                         "}";
+
+  Serial.print("JSON: ");
+  Serial.println(telemetryJson);
+
   bool res = mqttClient.publish("v1/gateway/connect", deviceJson.c_str());
   Serial.printf("Device connected? %i\n", res);
 
@@ -225,7 +247,8 @@ void postData(char data[])
   res = mqttClient.publish("v1/gateway/disconnect", deviceJson.c_str());
   Serial.printf("Device disconnected? %i\n", res);
 
-  if (mqttClient.connected()) mqttClient.disconnect();
+  if (mqttClient.connected())
+    mqttClient.disconnect();
 }
 
 // int postData(char data[5])
